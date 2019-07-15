cfe53149548aaa4af6f089ab2a6b119697073374
aashu <aashutosh.ayush@gmail.com>
2019-07-15 11:48:19 +0000

    15th July Maven & WebApp

---
+++ productapp/src/main/java/com/visa/prj/client/AddProduct.java
@@ -0,0 +1,23 @@
+package com.visa.prj.client;
+
+import com.visa.prj.dao.PersistenceException;
+import com.visa.prj.dao.ProductDao;
+import com.visa.prj.dao.ProductDaoJdbcImpl;
+import com.visa.prj.entity.Product;
+
+public class AddProduct {
+
+	public static void main(String[] args) {
+		ProductDao productDao = new ProductDaoJdbcImpl();
+
+		try {
+			Product p = new Product(0, "Logitech mouse", "computer", 400.00, 500);
+			int id = productDao.addProduct(p);
+//			productDao.addProduct(p);
+			System.out.println("Product added with id "+ id);
+		} catch (PersistenceException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
---
+++ productapp/src/main/java/com/visa/prj/dao/ProductDaoJdbcImpl.java
@@ -0,0 +1,66 @@
+package com.visa.prj.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.visa.prj.entity.Product;
+
+public class ProductDaoJdbcImpl implements ProductDao {
+
+//	@Override
+//	public int addProduct(Product p) throws PersistenceException {
+//		return 0;
+//	}
+	@Override
+	public int addProduct(Product p) throws PersistenceException {
+		int id = 0;
+		String SQL = "insert into products(id, name, price, category, count) values (0, ?, ?, ?, ?)";
+		Connection con = null;
+		try {
+			con = DbUtil.getConnection();
+			PreparedStatement ps = con.prepareStatement(SQL, Statement.RETURN_GENERATED_KEYS); 
+			ps.setString(1, p.getName());
+			ps.setDouble(2, p.getPrice());
+			ps.setString(3, p.getCategory());
+			ps.setInt(4, p.getCount());
+			ps.executeUpdate(); // INSERT, DELETE, UPDATE
+			ResultSet rs = ps.getGeneratedKeys();
+			if(rs.next()) {
+//				id = rs.getInt("id");
+				id = rs.getInt(1);
+
+			}
+		} catch (SQLException ex) {
+			throw new PersistenceException("unable to add product", ex); //here wrapping the original exception while rethrowing the exception
+		} finally {
+			DbUtil.closeConnection(con);
+		}
+		return id;
+	}
+
+	@Override
+	public List<Product> getProducts() throws FetchException {
+		List<Product> products = new ArrayList<>();
+		String SQL = "select * from products";
+		Connection con = null;
+		try {
+			con = DbUtil.getConnection();
+			Statement stmt = con.createStatement();
+			ResultSet rs = stmt.executeQuery(SQL); //SELECT
+			while (rs.next()) {
+				products.add(new Product(rs.getInt("id"), rs.getString("name"), rs.getString("category"),
+						rs.getDouble("price"), rs.getInt("count")));
+			}
+		} catch (SQLException e) {
+			throw new FetchException("unable to get product", e);
+		} finally {
+			DbUtil.closeConnection(con);
+		}
+		return products;
+	}
+}
